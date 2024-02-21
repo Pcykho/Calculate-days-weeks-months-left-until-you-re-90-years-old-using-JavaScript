14e11fa310ba20179afc393957757506849296c7
Pcykho Duke <62355234+Pcykho@users.noreply.github.com>
2024-02-21 17:34:39 +0530

    added JS files

---
+++ Calculate days, weeks, months left until you're 90 years old using JavaScript.js
@@ -0,0 +1,13 @@
+/* How many days, weeks and months left until you're 90 years old.
+Special thanks to Angela Yu @ AppBrewary */
+
+var age = prompt ("What's your current age?");
+
+    var lifeInWeeks = (prompt = ("Your current age:"));
+    var yearsLeft = ( 90 - age );
+    var monthsLeft = ( yearsLeft * 12 );
+    var weeksLeft = ( yearsLeft * 52 );
+    var daysLeft = ( yearsLeft * 365 );
+    
+    alert ("You have " + daysLeft + " days, " + weeksLeft + " weeks , and " + monthsLeft + " months left.");
+        
